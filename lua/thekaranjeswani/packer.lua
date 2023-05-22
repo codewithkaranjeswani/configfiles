15fff7587b534fca66eb28a5096224d486d0fcc5
codewithkaranjeswani <krnjeswani21@gmail.com>
2023-05-22 18:04:03 +0530

    undotree and fugitive

--- lua/thekaranjeswani/packer.lua
+++ lua/thekaranjeswani/packer.lua
@@ -26,6 +26,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/playground')
   use('nvim-lua/plenary.nvim')
   use('theprimeagen/harpoon')
-
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 end)
 
